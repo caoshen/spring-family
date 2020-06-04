e462dc10d35cbc8e87f70a6f6c162f053fcb128c
caoshen <ceeseemail@gmail.com>
2020-06-04 23:58:23 +0800

    add handler

--- chapter3/mybatis-demo/src/main/java/com/example/mybatisdemo/MybatisDemoApplication.java
+++ chapter3/mybatis-demo/src/main/java/com/example/mybatisdemo/MybatisDemoApplication.java
@@ -1,13 +1,45 @@
 package com.example.mybatisdemo;
 
+import com.example.mybatisdemo.mapper.CoffeeMapper;
+import com.example.mybatisdemo.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class MybatisDemoApplication {
+@MapperScan("com.example.mybatisdemo.mapper")
+public class MybatisDemoApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybatisDemoApplication.class, args);
-	}
+    @Autowired
+    private CoffeeMapper coffeeMapper;
 
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemoApplication.class, args);
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Coffee c = Coffee.builder().name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .build();
+        int count = coffeeMapper.save(c);
+        log.info("Save {} Coffee: {}", count, c);
+
+        c = Coffee.builder().name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.0))
+                .build();
+        count = coffeeMapper.save(c);
+        log.info("Save {} Coffee: {}", count, c);
+
+        c = coffeeMapper.findById(c.getId());
+        log.info("Find Coffee: {}", c);
+    }
 }
